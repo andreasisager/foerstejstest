105176c28ba7bde9b040726544c8ec2661868a00
andreasisager <andreas.isager@gmail.com>
2025-02-04 09:00:58 +0100

    ojkjk

--- eksternt.js
+++ eksternt.js
@@ -1,8 +1,18 @@
 
 //Så mit navn står i consolen
 console.log("Andreas");
-//Denne H1 overtager hele min body
-document.body.innerHTML = "<h1>Andreasermitnavn</h1>";
+//Denne H1 overtager hele min body (men er nu sat som kommentar)
+//document.body.innerHTML = "<h1>Andreasermitnavn</h1>";
+
+document.getElementById("demo").innerHTML = z
+document.getElementById("demo").style.color = "red";
+
+var x = 2
+var y = 4
+var z = x + y
+
+const frugt = ["kiwi", "banan", "vandmelon"]
+getElementById("demo").innerHTML = frugt
 
 
 
